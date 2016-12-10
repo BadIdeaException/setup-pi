080df35e36edb6405e8fea58bd82f3f248ec4b1f
chris <tigerman33@gmx.net>
2016-12-10 19:36:19 +0100

    Server start script including workaround for containers not starting more than once due to apache2.pid still being present

---
+++ resources/start-server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set environment variables needed by apache
+source /etc/apache2/envvars
+
+# If this is not the first start of this container, it won't start again if the pid file is still present
+# So delete it
+rm -f $APACHE_PID_FILE
+
+/usr/sbin/apache2 -DFOREGROUND
