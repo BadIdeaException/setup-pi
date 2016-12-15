e04050022c53f5586e84baf0cc678c19825af5cf
chris <tigerman33@gmx.net>
2016-12-15 18:15:53 +0100

    Persist MYSQL_ROOT_PASSWORD

--- scripts/20-install-mysql.sh
+++ scripts/20-install-mysql.sh
@@ -9,11 +9,13 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # Path to resources folder
-RESOURCE_LOCATION=$(dirname "$(readlink -f \"$0\")")/../resources/mysql
+RESOURCE_LOCATION=$(dirname "$(readlink -f $0)")/../resources/mysql
 
 if [ ! $MYSQL_ROOT_PASSWORD ]; then
    echo "A MySQL root password has not been set. Please enter one now and make sure you remember it:"
    read MYSQL_ROOT_PASSWORD
+   # Persist this variable
+   echo "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" >> /etc/environment
 fi
 
 mkdir --parents /var/vol/mysql/dumps
