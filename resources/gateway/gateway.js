0b9a45726d5120bec87f4782ee16997ad1995991
chris <tigerman33@gmx.net>
2017-07-08 13:37:23 +0200

    Added help text

--- resources/gateway/gateway.js
+++ resources/gateway/gateway.js
@@ -2,7 +2,8 @@
 var proxy = require('redbird')({ port: 80, bunyan: false });
 var fs = require('fs');
 
-function processArgs(argArray) {
+// Process command line arguments from the original array form into a hash of the form option: value
+var args = function processArgs(argArray) {
 	var result = {};
 	argArray
 		.slice(2) // Cut off the first two parameters (node invocation and this script file as an argument to node)
@@ -10,24 +11,34 @@ function processArgs(argArray) {
 			arg = arg.split('='); // Arguments are in the form option=value, split current one into array [option,value]
 			result[arg[0]] = arg[1];
 		});
+	return result;
+}(process.argv);
+
+var sitepath = '/etc/redbird/sites.d/' || args['sitepath']; // Override default site path from command line
+if (!sitepath.endsWith('/')) { sitepath += '/' };
+
+
+console.log('Redbird based gateway\n');
+
+// Print help text if run with --help
+if (args.hasOwnProperty('help')) {
+	console.log(
+		'You can specify the following command line options: \n' + 
+		'    --sitepath=PATH        Read site definitions from PATH instead of standard /etc/redbird/sites.d\n' +
+		'    --help                 Display this help message'
+	);
 }
-var args = processArgs(process.argv);
 
-
-var configpath = '/etc/redbird/sites.d/';
-
-
-console.log('Starting Redbird based gateway');
-console.log('Reading sites from ' + configpath);
+console.log('Reading sites from ' + sitepath);
 
 fs
 	// Read configuration directory
-	.readdirSync(configpath)
+	.readdirSync(sitepath)
 	// Filter only files with the .site extension
 	.filter(function(file) { return file.indexOf('.site') !== 0; })
 	// 
 	.forEach(function(file) {
-		var config = JSON.parse(fs.readFileSync(configpath + '/' + file)); // Load a file
+		var config = JSON.parse(fs.readFileSync(sitepath + file)); // Load a file
 		console.log('Registering site ' + config.domain + ' to be proxied to ' + config.target);
 		proxy.register(config.domain, config.target, config.options);
 	});
