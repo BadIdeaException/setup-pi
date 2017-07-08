4102d68c62ebffd291ce381669870b1ae7b09fb7
chris <tigerman33@gmx.net>
2017-07-08 12:52:48 +0200

    Added gateway.js (Redbird-based reverse proxying) and associated package.json

---
+++ resources/gateway/gateway.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+var proxy = require('redbird')({ port: 80 });
+var fs = require('fs');
+
+var configpath = '/etc/redbird/sites.d/';
+
+fs
+	// Read configuration directory
+	.readdirSync(configpath)
+	// Filter only files with the .site extension
+	.filter(function(file) { file.indexOf('.site') !== 0; })
+	// 
+	.forEach(function(file) {
+		var config = require(configpath + '/' + file); // Load a file
+		console.log('Registering site ' + config.domain + ' to be routed to ' + config.target);
+		proxy.register(config.domain, config.target, config.options);
+	});
