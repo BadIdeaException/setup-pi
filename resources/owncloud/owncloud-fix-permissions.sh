749b39aa5807eaebabb46be1da36043eb71de19c
chris <tigerman33@gmx.net>
2017-07-08 16:33:10 +0200

    Increased verbosity

--- resources/owncloud/owncloud-fix-permissions.sh
+++ resources/owncloud/owncloud-fix-permissions.sh
@@ -7,19 +7,19 @@ HTGROUP=${HTUSER:-'www-data'}
 ROOTUSER='root'
 
 
-find ${OCPATH}/ -type f -print0 | xargs -0 chmod 0640
-find ${OCPATH}/ -type d -print0 | xargs -0 chmod 0750
+find ${OCPATH}/ -type f -print0 | xargs -0 chmod --verbose 0640
+find ${OCPATH}/ -type d -print0 | xargs -0 chmod --verbose 0750
 
 echo "Changing owner of ${OCPATH}/ to ${ROOTUSER}:${HTGROUP}"
-chown -R ${ROOTUSER}:${HTGROUP} ${OCPATH}/
+chown --recursive --verbose ${ROOTUSER}:${HTGROUP} ${OCPATH}/
 echo "Changing owner of ${OCPATH}/apps/ to ${HTUSER}:${HTGROUP}"
-chown -R ${HTUSER}:${HTGROUP} ${OCPATH}/apps/
+chown --recursive --verbose ${HTUSER}:${HTGROUP} ${OCPATH}/apps/
 echo "Changing owner of ${OCPATH}/config/ to ${HTUSER}:${HTGROUP}"
-chown -R ${HTUSER}:${HTGROUP} ${OCPATH}/config/
+chown --recursive --verbose ${HTUSER}:${HTGROUP} ${OCPATH}/config/
 echo "Changing owner of ${DATAPATH}/ to ${HTUSER}:${HTGROUP}"
-chown -R ${HTUSER}:${HTGROUP} ${DATAPATH}/
+chown --recursive --verbose ${HTUSER}:${HTGROUP} ${DATAPATH}/
 echo "Changing owner of ${OCPATH}/themes/ to ${HTUSER}:${HTGROUP}"
-chown -R ${HTUSER}:${HTGROUP} ${OCPATH}/themes/
+chown --recursive --verbose ${HTUSER}:${HTGROUP} ${OCPATH}/themes/
 
 echo "Changing owner of ${OCPATH}/.htaccess to ${ROOTUSER}:${HTGROUP}"
 chown ${ROOTUSER}:${HTGROUP} ${OCPATH}/.htaccess || true # Prevent non-zero exit code if no .htaccess file present
@@ -27,6 +27,6 @@ echo "Changing owner of ${DATAPATH}/.htaccess to ${ROOTUSER}:${HTGROUP}"
 chown ${ROOTUSER}:${HTGROUP} ${DATAPATH}/.htaccess || true # Prevent non-zero exit code if no .htaccess file present
 
 echo "Changing permissions of ${OCPATH}/.htaccess to 0644"
-chmod 0644 ${OCPATH}/.htaccess || true # Prevent non-zero exit code if no .htaccess file present
+chmod --verbose 0644 ${OCPATH}/.htaccess || true # Prevent non-zero exit code if no .htaccess file present
 echo "Changing permissions of ${DATAPATH}/.htaccess to 0644"
-chmod 0644 ${DATAPATH}/.htaccess || true # Prevent non-zero exit code if no .htaccess file present
+chmod --verbose 0644 ${DATAPATH}/.htaccess || true # Prevent non-zero exit code if no .htaccess file present
